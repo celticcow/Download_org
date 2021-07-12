02b273e5eb3ab5670d7be61a894a25dfdbe5de4e
Gregory Dunlap <gregory.dunlap@fedex.com>
2021-07-12 18:06:47 -0500

    test code for magic

---
+++ test_functions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import os
+import magic
+
+#sudo apt-get install python3-magic
+
+def main():
+    directory_to_check = "/home/gdunlap/Downloads"
+    files = os.listdir(directory_to_check)
+
+    for f in files:
+        print(f)
+        my_file = directory_to_check + "/" + f
+        info = magic.from_file(my_file)
+        #info = magic.from_file(f)
+        if "image" in info:
+            print("IMAGE FOUND")
+        
+        print(info)
+        print("---------------------------")
+
+if __name__ == "__main__":
+    main()
