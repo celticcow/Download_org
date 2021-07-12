425b2247d3219c11483b961ef8f0fc5464175a45
Gregory Dunlap <gregory.dunlap@fedex.com>
2021-07-12 18:07:13 -0500

    can move files .. not pretty or organied yet

--- download_org.py
+++ download_org.py
@@ -1,3 +1,35 @@
 #!/usr/bin/python3
 
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+#pip3 install watchdog
+#sudo apt-get install python3-magic
 
+import os
+import json
+import time
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        for filename in os.listdir(folder_to_track):
+            src = folder_to_track + "/" + filename
+            new_destination = folder_destination + "/" + filename
+            print(filename)
+            os.rename(src, new_destination)
+
+
+folder_to_track = "/home/gdunlap/Desktop/myFolder"
+folder_destination = "/home/gdunlap/Desktop/newfolder"
+
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
