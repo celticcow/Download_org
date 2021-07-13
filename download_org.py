3c01ad3f9783371a1a56847cd94feba6f0c5117f
Gregory Dunlap <gregory.dunlap@fedex.com>
2021-07-12 20:59:41 -0500

    added constructor to class and main function

--- download_org.py
+++ download_org.py
@@ -8,28 +8,68 @@ from watchdog.events import FileSystemEventHandler
 import os
 import json
 import time
+import magic
 
-class MyHandler(FileSystemEventHandler):
+"""
+download handler that inhearttes from file system event handler
+"""
+class DownloadHandler(FileSystemEventHandler):
+    #constructor
+    #worried about the /home/<user>/Downloads
+    def __init__(self, folder_to_track):
+        self.folder_to_track = folder_to_track
+        #self.dest_root = dest_root
+
     def on_modified(self, event):
-        for filename in os.listdir(folder_to_track):
-            src = folder_to_track + "/" + filename
-            new_destination = folder_destination + "/" + filename
-            print(filename)
-            os.rename(src, new_destination)
+        debug = 1
+        for filename in os.listdir(self.folder_to_track):
+            src = self.folder_to_track + "/" + filename
+
+            try:
+                file_info = magic.from_file(src)
+
+                """
+                need to make a function for sub folder /type and os.rename
+                """
+                if "IMAGE" in file_info.upper():
+                    if(debug == 1):
+                        print("found image") #debug 
+                    new_destination = self.folder_to_track + "/images/" + filename
+                    os.rename(src, new_destination)
+                elif "PDF" in file_info.upper():
+                    if(debug == 1):
+                        print("found pdf") 
+                    new_destination = self.folder_to_track + "/pdf/" + filename
+                    os.rename(src, new_destination)
+                elif "WORD" in file_info.upper():
+                    if(debug == 1):
+                        print("found word doc") 
+                    new_destination = self.folder_to_track + "/word/" + filename
+                    os.rename(src, new_destination)
+            except IsADirectoryError:
+                pass
+            #new_destination = folder_destination + "/" + filename
+            #print(filename)
+            #os.rename(src, new_destination)
 
 
-folder_to_track = "/home/gdunlap/Desktop/myFolder"
-folder_destination = "/home/gdunlap/Desktop/newfolder"
+def main():
+    folder_to_track = "/home/gdunlap/Desktop/myFolder"
+    #folder_destination = "/home/gdunlap/Desktop/newfolder"
 
-event_handler = MyHandler()
-observer = Observer()
-observer.schedule(event_handler, folder_to_track, recursive=True)
-observer.start()
+    event_handler = DownloadHandler(folder_to_track)
+    observer = Observer()
+    observer.schedule(event_handler, folder_to_track, recursive=True)
+    observer.start()
 
-try:
-    while True:
-        time.sleep(10)
-except KeyboardInterrupt:
-    observer.stop()
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
 
-observer.join()
+    observer.join()
+
+if __name__ == "__main__":
+    main()
+#end of download_org
